db34c4b28f9aefba94924da0bbd9cf5a1766312c
maciejspychala <mac.spychala@gmail.com>
2017-03-10 22:08:39 +0100

    adds file chooser

--- app/Main.hs
+++ app/Main.hs
@@ -22,9 +22,9 @@ main = do
     void initGUI
     window <- windowNew
     set window [ windowTitle := "CTApp"
-               , windowDefaultWidth := 640
-               , windowDefaultHeight := 480 ]
-    table <- tableNew 2 3 True
+               , windowDefaultWidth := 1280
+               , windowDefaultHeight := 768 ]
+    table <- tableNew 3 3 True
     containerAdd window table
     buttonOriginal <- buttonNewWithLabel "original"
     buttonTransformed <- buttonNewWithLabel "transformed"
@@ -38,16 +38,16 @@ main = do
     tableAttachDefaults table buttonOriginal 0 1 0 1
     tableAttachDefaults table buttonTransformed 0 1 1 2
     tableAttachDefaults table buttonDiff 0 1 2 3
-    tableAttachDefaults table buttonQuit 1 2 0 1
 
 
     boxWithSpins <- hBoxNew True 10
 
     raysSpin <- addSpinButton boxWithSpins "Rays number" 100.0 500.0
     scansSpin <- addSpinButton boxWithSpins "Scans number" 5.0 100.0
+    tableAttachDefaults table boxWithSpins 1 2 2 3
 
-    
-    tableAttachDefaults table boxWithSpins 1 2 1 2
+    fileChooser <- fileChooserWidgetNew FileChooserActionOpen
+    tableAttachDefaults table fileChooser 1 3 0 2
 
     onDestroy window mainQuit
     widgetShowAll window
